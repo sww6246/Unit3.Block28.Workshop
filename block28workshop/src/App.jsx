1cb5b2ebe4dddff7ad3c836a15cb979467aeb387
sww6246 <sam.weber7@gmail.com>
2025-04-15 06:54:20 -0600

    HW Commit

---
+++ block28workshop/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Red from './Red.jsx'
+import Blue from './Blue.jsx'
+import Home from './Home.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div id="container">
+      <div id="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/blue" element={<Blue/>} />
+          <Route path="/red" element={<Red/>} />
+        </Routes>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
